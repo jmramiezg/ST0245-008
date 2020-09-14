17be2a0bd599f7bf10f259a216299313dc19378e
jmramiezg <69641352+jmramiezg@users.noreply.github.com>
2020-09-14 17:02:38 -0500

    Add files via upload

---
+++ proyecto/primera_entrega.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+T0 = pd.read_csv("TEST 0.csv")
+T1 = pd.read_csv("TEST 1.csv")
+T2 = pd.read_csv("TEST 2.csv")
+T3 = pd.read_csv("TEST 3.csv")
+T4 = pd.read_csv("TEST 4.csv")
+Tlite = pd.read_csv('TEST lite.csv')
+Tr0 = pd.read_csv("TRAIN 0.csv")
+Tr1 = pd.read_csv("TRAIN 1.csv")
+Tr2 = pd.read_csv("TRAIN 2.csv")
+Tr3 = pd.read_csv("TRAIN 3.csv")
+Tr4 = pd.read_csv("TRAIN 4.csv")
+
+print(Tlite) 
+
